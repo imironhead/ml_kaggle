1e7dc365b8085c72dd64ecf491e94d02b25a8bf0
ironhead <ironhead.chuang@gmail.com>
2018-10-21 10:50:48 +0800

    - try self-attention on cnn model

--- qdraw/qdraw/model_cnn.py
+++ qdraw/qdraw/model_cnn.py
@@ -1,54 +1,119 @@
 """
 """
-import numpy as np
 import tensorflow as tf
 
 
-def build_model(images, labels):
+def self_attention(tensors, num_out_channels, num_aux_channels, scope_name):
     """
-    images: [None, 28, 28, 1] float32
+    """
+    shape = tf.shape(tensors)
+
+    batch_size, height, width = [shape[i] for i in range(3)]
+
+    initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+    with tf.variable_scope(scope_name):
+        fx = tf.layers.conv2d(
+            tensors,
+            filters=num_aux_channels,
+            kernel_size=1,
+            strides=1,
+            padding='same',
+            data_format='channels_last',
+            activation=tf.nn.relu,
+            use_bias=True,
+            kernel_initializer=initializer,
+            name='fx')
+
+        gx = tf.layers.conv2d(
+            tensors,
+            filters=num_aux_channels,
+            kernel_size=1,
+            strides=1,
+            padding='same',
+            data_format='channels_last',
+            activation=tf.nn.relu,
+            use_bias=True,
+            kernel_initializer=initializer,
+            name='gx')
+
+        hx = tf.layers.conv2d(
+            tensors,
+            filters=num_out_channels,
+            kernel_size=1,
+            strides=1,
+            padding='same',
+            data_format='channels_last',
+            activation=tf.nn.relu,
+            use_bias=True,
+            kernel_initializer=initializer,
+            name='hx')
+
+        gamma = tf.get_variable(
+            'gamma',
+            [],
+            dtype=tf.float32,
+            initializer=tf.constant_initializer(0.0))
+
+        fx = tf.reshape(fx, [batch_size, width * height, num_aux_channels])
+        gx = tf.reshape(gx, [batch_size, width * height, num_aux_channels])
+        hx = tf.reshape(hx, [batch_size, width * height, num_out_channels])
+        mx = tf.matmul(fx, gx, transpose_b=True)
+        mx = tf.nn.softmax(mx, axis=2)
+        o = tf.matmul(mx, hx)
+        o = tf.reshape(o, [batch_size, height, width, num_out_channels])
+
+        tensors = gamma * o + tensors
+
+    return tensors
+
+
+def build_model(images, strokes, lengths, labels):
+    """
+    images: [None, size, size, 1] float32
     labels: [None], 340 categories
     """
     initializer = tf.truncated_normal_initializer(stddev=0.02)
 
     tensors = images
 
-    for index, filters in enumerate([32, 64, 128, 256]):
+    tensors = tf.layers.conv2d(
+        tensors,
+        filters=32,
+        kernel_size=3,
+        strides=1,
+        padding='same',
+        data_format='channels_last',
+        activation=tf.nn.relu,
+        use_bias=True,
+        kernel_initializer=initializer,
+        name='conv_in')
+
+    tensors = self_attention(tensors, 32, 4, 'conv_in_attention')
+
+    for index, filters in enumerate([64, 128, 256]):
         tensors = tf.layers.conv2d(
             tensors,
             filters=filters,
             kernel_size=3,
-            strides=1,
-            padding='valid',
+            strides=2,
+            padding='same',
             data_format='channels_last',
             activation=tf.nn.relu,
             use_bias=True,
             kernel_initializer=initializer,
             name='conv_{}'.format(index))
 
-        # NOTE: norm ?
+        tensors = self_attention(
+            tensors, filters, filters // 8, 'conv_{}_attention'.format(index))
 
-    # NOTE: position tag
-#   x = np.linspace(-1.0, 1.0, 28, dtype=np.float32)
-#   u, v = np.meshgrid(x, x)
-
-#   u = np.reshape(u, [1, 28, 28, 1])
-#   v = np.reshape(v, [1, 28, 28, 1])
-
-#   coordinates = np.concatenate([u, v], axis=-1)
-#   coordinates = tf.constant(coordinates)
-
-#   n = tf.shape(tensors)[0]
-
-#   coordinates = tf.tile(coordinates, [n, 1, 1, 1])
-
-#   tensors = tf.concat([tensors, coordinates], axis=-1)
+    embeddings = tensors
 
     # NOTE: flatten for fc
-    tensors = tf.layers.flatten(tensors)
+    tensors = tf.layers.flatten(embeddings)
 
     # NOTE
-    for i, units in enumerate([2048, 2048, 512]):
+    for i, units in enumerate([2048, 1024]):
         tensors = tf.layers.dense(
             tensors,
             units=units,
@@ -62,7 +127,7 @@ def build_model(images, labels):
         tensors,
         units=340,
         activation=None,
-        use_bias=False,
+        use_bias=True,
         kernel_initializer=initializer,
         name='final')
 
@@ -72,13 +137,47 @@ def build_model(images, labels):
     if labels is None:
         return {
             'images': images,
+            'strokes': strokes,
+            'lengths': lengths,
             'logits': logits,
         }
 
-    loss = tf.losses.sparse_softmax_cross_entropy(
+    loss_classification = tf.losses.sparse_softmax_cross_entropy(
         labels,
         logits,
         reduction=tf.losses.Reduction.SUM_OVER_BATCH_SIZE)
+
+    tensors = embeddings
+
+    for index, filters in enumerate([128, 64, 32]):
+        tensors = tf.layers.conv2d_transpose(
+            tensors,
+            filters=filters,
+            kernel_size=3,
+            strides=2,
+            padding='same',
+            data_format='channels_last',
+            activation=tf.nn.relu,
+            use_bias=True,
+            kernel_initializer=initializer,
+            name='dconv_{}'.format(index))
+
+    tensors = tf.layers.conv2d(
+        tensors,
+        filters=1,
+        kernel_size=3,
+        strides=1,
+        padding='same',
+        data_format='channels_last',
+        activation=tf.nn.tanh,
+        use_bias=True,
+        kernel_initializer=initializer,
+        name='conv_out')
+
+    loss_reconstruction = tf.losses.mean_squared_error(
+        images, tensors, reduction=tf.losses.Reduction.SUM_OVER_BATCH_SIZE)
+
+    loss = loss_classification + loss_reconstruction
 
     step = tf.train.get_or_create_global_step()
 
@@ -91,6 +190,8 @@ def build_model(images, labels):
     return {
         'images': images,
         'labels': labels,
+        'strokes': strokes,
+        'lengths': lengths,
         'logits': logits,
         'loss': loss,
         'step': step,
